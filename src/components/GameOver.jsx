1562ecba329b9603168b5bca5bb8c13f4ffac985
Sarah Luo <sarrstar888@gmail.com>
2024-03-02 13:56:37 -0500

    udpating game over

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,5 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { AppContext } from "./Game";
+import { supabase } from "../supabaseClient";
+import { UserContext } from "../App";
+
+async function getDefinition(word) {
+  const { data, error } = await supabase.from("word-table")
+  .select('definition')
+  .eq('word', word)
+
+  console.log(error);
+  console.log(word)
+  console.log(data);
+  return data[0].definition;
+
+}
 
 function GameOver() {
   const {
@@ -11,6 +25,16 @@ function GameOver() {
     correctWord,
     onDelete,
   } = useContext(AppContext);
+  const [definition, setDefinition] = useState(null);
+
+  useEffect(() => {
+    async function fetchDefinition() {
+      const definition = await getDefinition(correctWord);
+      setDefinition(definition);
+    }
+    fetchDefinition();
+  }, [correctWord]);
+
   return (
     <div className="gameOver">
       <h3>
@@ -19,6 +43,7 @@ function GameOver() {
           : "You Failed to Guess the Word"}
       </h3>
       <h1>Correct Word: {correctWord}</h1>
+      <h1>Definition: {definition}</h1>
       {gameOver.guessedWord && (
         <h3>You guessed in {currAttempt.attempt} attempts</h3>
       )}
